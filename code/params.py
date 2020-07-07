104828b7de283e369e1a6a0808687019314bc8ef
Leann Thayaparan <leann.tparan@gmail.com>
2020-07-07 12:40:53 -0400

    updated SIR model

--- code/params.py
+++ code/params.py
@@ -10,10 +10,12 @@ from datetime import datetime
 
 #%% User and path
 
-USER = 'omar'
+USER = 'lpgt'
 
 if USER == 'omar':
     df_path = r'C:\Users\david\Desktop\MIT\Courses\Research internship\covid19_team2\data\input\06_15_2020_states_combined.csv'
+elif USER == 'lpgt':
+    df_path = r'../data/input/06_15_2020_MA_only.csv'
 
 #%% Target and column names
 
@@ -27,17 +29,17 @@ nmin = 100
 
 random_state = 42
 training_cutoff = '2020-05-25'
-regions = ['New York', 'Massachusetts'] # regions to predict
+regions = ['Massachusetts'] # regions to predict
 unformatted_dates = ['2020-07-01', '2020-08-15'] # dates to predict
 
 #%% Load and Save Parameters
 
 train_knn = False
-train_mdp = True
-train_sir = False
+train_mdp = False
+train_sir = True
 load_knn = False
-load_mdp = True
-load_sir = False
+load_mdp = False
+load_sir = True
 sir_file = 'sir.pickle'
 knn_file = 'knn.pickle'
 mdp_file = 'mdp.pickle'
@@ -48,7 +50,11 @@ sir_params_dict = \
     {
         "nmin": 100,
         "optimizer": 'Nelder-Mead',
-        "initial_param": [0.4, 0.06],
+        "betavals": [0.10, 0.15, 0.9, 0.95, 1.1, 1.2],
+        "gammavals": [0.01, 0.03, 0.25, 0.27, 0.29],
+        "avals": [0.333, 0.142, 0.0909, 0.0714, 0.0526],
+        "muvals": [0.001, 0.003, 0.005, 0.007],
+        "train_valid_split": 0.8,
         "nmin_train_set": 10
     }
 #%% Parameters KNN
